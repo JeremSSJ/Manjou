051500979603cb9a17ac7796a00199808de346c1
JeremSSJ <jeremssj7@gmail.com>
2022-07-11 22:25:24 +0200

    guiguipdx

--- view/home.php
+++ view/home.php
@@ -1,7 +1,7 @@
 <?php
 
 echo '<h2>vous etes sur la page home</h2>';
-echo '<h2>guigui pd</h2>';
+echo '<h2>guigui pdx</h2>';
 ?>
 <a href="<?= $router->generate('test') ?>">Test</a>
 <a href="<?= $router->generate('page') ?>">Page</a>
