c9e5242b5328948af307df30d93878cb08906c17
JeremSSJ <jeremssj7@gmail.com>
2022-07-12 11:49:16 +0200

    enlever fichier css

--- view/elements/header.php
+++ view/elements/header.php
@@ -1,14 +1,19 @@
 <!DOCTYPE html>
 <html lang="fr">
 
+<style>
+    .nav-color 
+    {
+        background-color: #940202;
+    }
+</style>
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- BOOTSTRAP CSS ET JS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
-    <!-- CSS PERSO -->
-    <link rel="stylesheet" href="../lib/style.css">
     <!-- ICONES -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css">
     <!-- JQUERY -->
@@ -57,3 +62,4 @@
             </div>
         </div>
     </nav>
+
