ca86fc1b53188c07670f62e0314f79a9d4bd5bb6
JeremSSJ <jeremssj7@gmail.com>
2022-07-12 16:28:00 +0200

    almost done signup design

--- view/elements/header.php
+++ view/elements/header.php
@@ -27,21 +27,21 @@
                 <form class="d-flex me-auto col-lg-4 mt-3 mt-lg-0" role="search">
                     <input class="form-control me-2" type="search" placeholder="Rechercher un restaurant ou un lieu" aria-label="Barre de recherche">
                     <button class="btn btn-warning" type="submit">
-                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
                         </svg>
                     </button>
                 </form>
                 
                 <a href="<?= $router->generate('inscription') ?>" class="btn btn-warning me-lg-2 px-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                         <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                         <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                     </svg>
                     Inscription
                 </a>
                 <a href="<?= $router->generate('connexion') ?>" class="btn btn-warning px-4">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-box-arrow-in-right" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
                         <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
                     </svg>
--- view/sign_in_up/sign_up.php
+++ view/sign_in_up/sign_up.php
@@ -16,20 +16,23 @@
 <body class="bg-light-red">
     <div class="container mt-5 pt-5">
         <div class="row">
-            <div class="col-12 col-md-8 col-lg-6 col-xl-5 m-auto">
+            <div class="col-12 col-md-8 col-lg-6 col-xl-4 m-auto">
                 <div class="card bg-red">
-                    <div class="card-body">
+                    <div class="card-body text-center">
+
+                        <h2 class="text-white-50 fw-bold mb-4">INSCRIPTION</h2>
+
                         <form action="">
-                            <input type="text" name="" id="" class="form-control my-4 py-2" placeholder="Nom d'utilisateur">
-                            <input type="email" name="" id="" class="form-control my-4 py-2" placeholder="E-mail">
-                            <input type="password" name="" id="" class="form-control my-4 py-2" placeholder="Mot de passe">
-                            <input type="password" name="" id="" class="form-control my-4 py-2" placeholder="Confirmer mot de passe">
+                            <input type="text" name="" id="" class="form-control form-control-lg my-4 py-2" placeholder="Nom d'utilisateur">
+                            <input type="email" name="" id="" class="form-control form-control-lg my-4 py-2" placeholder="E-mail">
+                            <input type="password" name="" id="" class="form-control form-control-lg my-4 py-2" placeholder="Mot de passe">
+                            <input type="password" name="" id="" class="form-control form-control-lg my-4 py-2" placeholder="Confirmer mot de passe">
 
-                            <div class="text-center mt-3">
-                                <button class="btn btn-warning fw-bold">S'inscrire</button>
+                            
+                            <button class="btn btn-lg btn-warning fw-bold px-4">S'inscrire</button>
 
-                                <p class="fw-bold">Vous avez déjà un compte ? <a href="" class="text-white-50 fw-bold">Se connecter</a></p>
-                            </div>
+                            <p class="fw-bold my-2 py-2">Vous avez déjà un compte ? <a href="" class="text-white-50 fw-bold">Se connecter</a></p>
+                            
                         </form>
                     </div>
                 </div>
