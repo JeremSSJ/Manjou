f2ade27b05010a8896bf3334423699c69023687f
JeremSSJ <jeremssj7@gmail.com>
2022-07-12 14:39:59 +0200

    sign up test

--- view/sign_in_up/sign_up.php
+++ view/sign_in_up/sign_up.php
@@ -2,9 +2,9 @@
 <?php require_once '../view/import/import.php'; ?>
 
 <style>
-    .bg-red-op
+    .bg-light-red
     {
-        background-color: #940202;
+        background-color: #420303;
     }
 
     .bg-red
@@ -13,7 +13,7 @@
     }
 </style>
 
-<body class="bg-red-op">
+<body class="bg-light-red">
     <div class="container mt-5 pt-5">
         <div class="row">
             <div class="col-12 col-sm-8 col-md-6 m-auto">
@@ -28,7 +28,7 @@
                             <div class="text-center mt-3">
                                 <button class="btn btn-warning">S'inscrire</button>
 
-                                <p>Vous avez déjà un compte ? <a href="" class="">Se connecter</a></p>
+                                <p class="text-black-50 fw-bold">Vous avez déjà un compte ? <a href="" class="text-white-50 fw-bold">Se connecter</a></p>
                             </div>
                         </form>
                     </div>
