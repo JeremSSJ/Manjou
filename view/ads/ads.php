15b2c9394671f4d1cc47762bc928ab57d5295848
JeremSSJ <jeremssj7@gmail.com>
2022-07-16 20:18:32 +0200

    debut interface restau

--- view/ads/ads.php
+++ view/ads/ads.php
@@ -1,41 +1,96 @@
 <?php require_once '../view/elements/header.php'; ?>
 
-<div class="container">
+<style>
+    .img-resize
+    {
+        object-fit: cover;
+        object-position: 50% 50%;
+    }
 
-    <div class="row mt-lg-4">
-        <div class="col-lg-3">
-            <div class="card">
-                <div class="card-body">
-                    <img src="/media/cc0-images/grapefruit-slice-332-332.jpg">
-                </div>
-            </div>
+</style>
+
+<div class="container-fluid ">
+    <div class="row mt-4 mx-3 gx-5 gy-5"><!-- mt-4 mx-3 gx-5 gy-5 -->
+
+        <div class="shadow col-lg-3">
+            <img class="h-100 img-resize img-fluid" src="img/1.png">
+            <h5 class="mt-2">Restaurant de la couille à Guillaume</h5>
         </div>
-        <div class="col-lg-3">
-            <div class="card">
-                <div class="card-body">
-                    <img src="/media/cc0-images/grapefruit-slice-332-332.jpg">
-                </div>
-            </div>
+        <div class="shadow col-lg-3">
+            <img class="h-100 img-resize img-fluid" src="img/2.jpg">
+            <h5 class="mt-2">Restaurant de la couille à Guillaume</h5>
         </div>
-        <div class="col-lg-3">
-            <div class="card">
-                <div class="card-body">
-                    <img src="/media/cc0-images/grapefruit-slice-332-332.jpg">
-                </div>
-            </div>
+        <div class="shadow col-lg-3">
+            <img class="h-100 img-resize img-fluid" src="img/3.jpg">
+            <h5 class="mt-2">Restaurant de la couille à Guillaume</h5>
         </div>
-        <div class="col-lg-3">
-            <div class="card">
-                <div class="card-body">
-                    <img src="/media/cc0-images/grapefruit-slice-332-332.jpg">
-                </div>
-            </div>
+        <div class="shadow col-lg-3">
+            <img class="h-100 img-resize img-fluid" src="img/4.jpg">
+            <h5 class="mt-2">Restaurant de la couille à Guillaume</h5>
+        </div>
+
+        <div class="shadow col-lg-3">
+            <img class="h-100 img-resize img-fluid" src="img/1.png">
+            <h5 class="mt-2">Restaurant de la couille à Guillaume</h5>
+        </div>
+        <div class="shadow col-lg-3">
+            <img class="h-100 img-resize img-fluid" src="img/2.jpg">
+            <h5 class="mt-2">Restaurant de la couille à Guillaume</h5>
+        </div>
+        <div class="shadow col-lg-3">
+            <img class="h-100 img-resize img-fluid" src="img/3.jpg">
+            <h5 class="mt-2">Restaurant de la couille à Guillaume</h5>
+        </div>
+        <div class="shadow col-lg-3">
+            <img class="h-100 img-resize img-fluid" src="img/4.jpg">
+            <h5 class="mt-2">Restaurant de la couille à Guillaume</h5>
+        </div>
+    </div>
+</div>
+
+
+
+<!-- 
+<div class="row">
+    <div class="col-lg-3 col-md-4">
+        <div class="box bg-primary h-100 d-flex p-4 flex-column text-white">
+            <img class="img img-fluid h-100" src="img/1.png">
+            <h1>Top</h1>
+            <p>ssssssssssssssssssssssss</p>
+            <h4>sssssssss</h4>
+            <a href="#" class="btn mt-auto btn-dark"></a>
+        </div>
+    </div>
+
+    <div class="col-lg-3 col-md-4">
+        <div class="box bg-primary h-100 d-flex p-4 flex-column text-white">
+            <img class="img img-fluid h-100" src="img/2.jpg">
+            <h1>Top</h1>
+            <p>ssssssssssssssssssssssss</p>
+            <a href="#" class="btn mt-auto btn-dark"></a>
         </div>
     </div>
 
 
+    <div class="col-lg-3 col-md-4">
+        <div class="box bg-primary h-100 d-flex p-4 flex-column text-white">
+            <img class="img img-fluid h-100" src="img/3.jpg">
+            <h1>Top</h1>
+            <p>ssssssssssssssssssssssss</p>
+            <a href="#" class="btn mt-auto btn-dark"></a>
+        </div>
+    </div>
 
+    <div class="col-lg-3 col-md-4">
+        <div class="box bg-primary  h-100 d-flex p-4 flex-column text-white">
+            <img class="img img-fluid h-100" src="img/4.jpg">
+            <h1>Top</h1>
+            <p>ssssssssssssssssssssssss</p>
+            <a href="#" class="btn mt-auto btn-dark"></a>
+        </div>
+    </div>
 </div>
+-->
 
 
 
