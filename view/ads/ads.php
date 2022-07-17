90b062a399fd85bdc1ceac2854e3b5d40c6df47b
JeremSSJ <jeremssj7@gmail.com>
2022-07-17 13:39:53 +0200

    fav debut

--- view/ads/ads.php
+++ view/ads/ads.php
@@ -7,13 +7,18 @@
         object-position: 50% 50%;
     }
 
+    .fav-pos
+    {
+        position: absolute;
+        color: #940202;
+    }
 </style>
 
 <div class="container-fluid ">
     <div class="row mx-1 mx-lg-3"><!-- mt-4 mx-3 gx-5 gy-5 -->
 
         <div class="col-lg-3 mt-4">
-            <div class="shadow card rounded-0 h-100">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce1">
                 <img class="img-resize h-100" src="img/1.png">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -24,12 +29,11 @@
                     </svg>
                     Mons
                 </div>
-                
             </div>
         </div>
 
         <div class="col-lg-3 mt-4">
-            <div class="shadow card rounded-0 h-100">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce2">
                 <img class="img-resize h-100" src="img/2.jpg">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -40,12 +44,11 @@
                     </svg>
                     Mons
                 </div>
-                
             </div>
         </div>
 
         <div class="col-lg-3 mt-4">
-            <div class="shadow card rounded-0 h-100">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce3">
                 <img class="img-resize h-100" src="img/3.jpg">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -56,12 +59,11 @@
                     </svg>
                     Mons
                 </div>
-                
             </div>    
         </div>
 
         <div class="col-lg-3 mt-4">
-            <div class="shadow card rounded-0 h-100">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce4">
                 <img class="img-resize h-100" src="img/4.jpg">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -72,13 +74,93 @@
                     </svg>
                     Mons
                 </div>
-               
             </div>
         </div>
 
+        <div class="col-lg-3 mt-4">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce1">
+                <img class="img-resize h-100" src="img/1.png">
+                <div class="card-body">
+                    <h5>Restaurant de la giga super couille à Guillaume</h5>
+                    <hr>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#940202" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                        <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg>
+                    Mons
+                </div>
+            </div>
+        </div>
 
+        <div class="col-lg-3 mt-4">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce2">
+                <img class="img-resize h-100" src="img/2.jpg">
+                <div class="card-body">
+                    <h5>Restaurant de la giga super couille à Guillaume</h5>
+                    <hr>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#940202" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                        <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg>
+                    Mons
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-3 mt-4">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce3">
+                <img class="img-resize h-100" src="img/3.jpg">
+                <div class="card-body">
+                    <h5>Restaurant de la giga super couille à Guillaume</h5>
+                    <hr>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#940202" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                        <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg>
+                    Mons
+                </div>
+            </div>    
+        </div>
+
+        <div class="col-lg-3 mt-4">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce4">
+                <img class="img-resize h-100" src="img/4.jpg">
+                <div class="card-body">
+                    <h5>Restaurant de la giga super couille à Guillaume</h5>
+                    <hr>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#940202" class="bi bi-geo-alt" viewBox="0 0 16 16">
+                        <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z"/>
+                        <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+                    </svg>
+                    Mons
+
+                </div>
+            </div>
+        </div>
     </div>
 </div>
 
 
 <?php require_once '../view/elements/footer.php'; ?>
+
+
+<script>
+    $(document).ready(() =>
+    {
+        //id dont le nom commence par 'annonce'
+        $("[id^=annonce]").hover(
+            
+            function()
+            {
+                $(this).removeClass('shadow-sm');
+                $(this).addClass('shadow');
+            },
+            function()
+            {
+                $(this).removeClass('shadow');
+                $(this).addClass('shadow-sm');
+            }
+        );
+    });
+
+</script>
