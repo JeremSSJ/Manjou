2839dcedceb7ff56a20277d6aaea37db82288d56
JeremSSJ <jeremssj7@gmail.com>
2022-07-17 16:41:24 +0200

    favoris suite

--- view/ads/ads.php
+++ view/ads/ads.php
@@ -7,18 +7,31 @@
         object-position: 50% 50%;
     }
 
+    .circle-color
+    {
+        background-color: #940202;
+    }
+
     .fav-pos
     {
         position: absolute;
-        color: #940202;
+        top: 2%;
+        right: 2%;
     }
 </style>
 
 <div class="container-fluid ">
-    <div class="row mx-1 mx-lg-3"><!-- mt-4 mx-3 gx-5 gy-5 -->
+    <div class="row mx-1 mx-lg-3">
 
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce1">
+                
+                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                </span>
+
                 <img class="img-resize h-100" src="img/1.png">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -34,6 +47,13 @@
 
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce2">
+
+                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                </span>
+
                 <img class="img-resize h-100" src="img/2.jpg">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -49,6 +69,13 @@
 
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce3">
+
+                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                </span>
+
                 <img class="img-resize h-100" src="img/3.jpg">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -64,6 +91,13 @@
 
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce4">
+
+                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                </span>
+
                 <img class="img-resize h-100" src="img/4.jpg">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -78,7 +112,14 @@
         </div>
 
         <div class="col-lg-3 mt-4">
-            <div class="shadow-sm card rounded-0 h-100" id="annonce1">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce5">
+
+                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                </span>
+
                 <img class="img-resize h-100" src="img/1.png">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -93,7 +134,13 @@
         </div>
 
         <div class="col-lg-3 mt-4">
-            <div class="shadow-sm card rounded-0 h-100" id="annonce2">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce6">
+
+            <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                </span>
                 <img class="img-resize h-100" src="img/2.jpg">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -108,7 +155,12 @@
         </div>
 
         <div class="col-lg-3 mt-4">
-            <div class="shadow-sm card rounded-0 h-100" id="annonce3">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce7">
+            <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                </span>
                 <img class="img-resize h-100" src="img/3.jpg">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -123,7 +175,12 @@
         </div>
 
         <div class="col-lg-3 mt-4">
-            <div class="shadow-sm card rounded-0 h-100" id="annonce4">
+            <div class="shadow-sm card rounded-0 h-100" id="annonce8">
+            <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
+                        <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
+                    </svg>
+                </span>
                 <img class="img-resize h-100" src="img/4.jpg">
                 <div class="card-body">
                     <h5>Restaurant de la giga super couille à Guillaume</h5>
@@ -145,22 +202,42 @@
 
 
 <script>
+    /*
     $(document).ready(() =>
     {
-        //id dont le nom commence par 'annonce'
-        $("[id^=annonce]").hover(
-            
-            function()
-            {
-                $(this).removeClass('shadow-sm');
-                $(this).addClass('shadow');
-            },
-            function()
-            {
-                $(this).removeClass('shadow');
-                $(this).addClass('shadow-sm');
-            }
-        );
-    });
+        
+    });*/
 
+    //id dont le nom commence par 'annonce'
+    $("[id^=annonce]").hover(
+            
+        function()
+        {
+            $(this).css('cursor', 'pointer');
+            $(this).removeClass('shadow-sm');
+            $(this).addClass('shadow');
+        },
+        function()
+        {
+            $(this).removeClass('shadow');
+            $(this).addClass('shadow-sm');
+        }
+    );
+
+    
+    $("[id^=annonce] > span").click(function()
+    {
+        if($(this).hasClass("empty"))
+        {
+            $(this).removeClass("empty");
+            $(this).addClass("filled");
+            $(this).html('<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star-fill" viewBox="0 0 16 16"><path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/></svg>');
+        }
+        else
+        {
+            $(this).removeClass("filled");
+            $(this).addClass("empty");
+            $(this).html('<svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16"><path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/></svg>');
+        }
+    });
 </script>
