db2c11db24db448b6d22488b27182b2080135264
JeremSSJ <jeremssj7@gmail.com>
2022-07-17 16:49:16 +0200

    favori fini sans bd

--- view/ads/ads.php
+++ view/ads/ads.php
@@ -9,7 +9,7 @@
 
     .circle-color
     {
-        background-color: #940202;
+        background-color: white;
     }
 
     .fav-pos
@@ -26,7 +26,7 @@
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce1">
                 
-                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                <span class="rounded-circle border border-dark fav-pos px-2 py-2 circle-color empty">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
                         <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                     </svg>
@@ -48,7 +48,7 @@
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce2">
 
-                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                <span class="rounded-circle border border-dark fav-pos px-2 py-2 circle-color empty">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
                         <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                     </svg>
@@ -70,7 +70,7 @@
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce3">
 
-                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                <span class="rounded-circle border border-dark fav-pos px-2 py-2 circle-color empty">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
                         <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                     </svg>
@@ -92,7 +92,7 @@
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce4">
 
-                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                <span class="rounded-circle border border-dark fav-pos px-2 py-2 circle-color empty">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
                         <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                     </svg>
@@ -114,7 +114,7 @@
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce5">
 
-                <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+                <span class="rounded-circle border border-dark fav-pos px-2 py-2 circle-color empty">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
                         <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                     </svg>
@@ -136,7 +136,7 @@
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce6">
 
-            <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+            <span class="rounded-circle border border-dark fav-pos px-2 py-2 circle-color empty">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
                         <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                     </svg>
@@ -156,7 +156,7 @@
 
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce7">
-            <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+            <span class="rounded-circle border border-dark fav-pos px-2 py-2 circle-color empty">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
                         <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                     </svg>
@@ -176,7 +176,7 @@
 
         <div class="col-lg-3 mt-4">
             <div class="shadow-sm card rounded-0 h-100" id="annonce8">
-            <span class="rounded-circle fav-pos px-2 py-2 circle-color empty">
+            <span class="rounded-circle border border-dark fav-pos px-2 py-2 circle-color empty">
                     <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="#EDB50C" class="bi bi-star" viewBox="0 0 16 16">
                         <path d="M2.866 14.85c-.078.444.36.791.746.593l4.39-2.256 4.389 2.256c.386.198.824-.149.746-.592l-.83-4.73 3.522-3.356c.33-.314.16-.888-.282-.95l-4.898-.696L8.465.792a.513.513 0 0 0-.927 0L5.354 5.12l-4.898.696c-.441.062-.612.636-.283.95l3.523 3.356-.83 4.73zm4.905-2.767-3.686 1.894.694-3.957a.565.565 0 0 0-.163-.505L1.71 6.745l4.052-.576a.525.525 0 0 0 .393-.288L8 2.223l1.847 3.658a.525.525 0 0 0 .393.288l4.052.575-2.906 2.77a.565.565 0 0 0-.163.506l.694 3.957-3.686-1.894a.503.503 0 0 0-.461 0z"/>
                     </svg>
